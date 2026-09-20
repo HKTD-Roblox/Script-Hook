822529d6681389d220fc5a6742d63d9d7dc32326
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-20 17:53:59 +0700

    Update JJS-Dream-Hub.lua

--- JJS-Dream-Hub.lua
+++ JJS-Dream-Hub.lua
@@ -1,4 +1,4 @@
-local url = "https://api.jnkie.com/api/v1/luascripts/public/b7315589ba3b7854c382eeb676a81db2be0007dc1b449c60236e38c6cc1ee3b7/download"
+local url = "https://api.jnkie.com/api/v1/luascripts/public/333967c3c665ff1d1548fa7766d57902756ac60eed4f1cc6e5eedce5d3133696/download"
 local success, result = pcall(function()
     return game:HttpGet(url)
 end)
