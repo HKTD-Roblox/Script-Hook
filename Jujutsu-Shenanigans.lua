8d22b039e58f63f01e93153abc40d813132ecd65
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-20 12:38:26 +0700

    Update Jujutsu-Shenanigans.lua

--- Jujutsu-Shenanigans.lua
+++ Jujutsu-Shenanigans.lua
@@ -1,1 +1,28 @@
-https://api.luarmor.net/files/v3/loaders/fda9babd071d6b536a745774b6bc681c.lua
+local url = "https://api.luarmor.net/files/v3/loaders/fda9babd071d6b536a745774b6bc681c.lua"
+local success, result = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success and result and #result > 0 then
+    if writefile then
+        writefile("Jujutsu-Shenanigans.lua", result)
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Jujutsu-Shenanigans",
+            Text = "Saved successfully to Jujutsu-Shenanigans.lua!",
+            Duration = 5
+        })
+    else
+        setclipboard(result)
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Jujutsu-Shenanigans",
+            Text = "Successfully copied to Clipboard!",
+            Duration = 5
+        })
+    end
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Jujutsu-Shenanigans",
+        Text = "Failed to download!",
+        Duration = 5
+    })
+end
